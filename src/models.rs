a179723c942b07750839e5956e027f8a2407e891
RudhyDev <rudhy.pereira@gmail.com>
2022-03-20 18:42:09 -0300

    feat: create NewBook table inside models

--- src/models.rs
+++ src/models.rs
@@ -2,9 +2,20 @@ use diesel;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 
+use schema::books;
+use schema::books::dsl::books as all_books;
+
 #[derive(Queryable)]
 pub struct Book {
   pub id: i32,
+  pub title: String,
+  pub author: String,
+  pub published: bool,
+}
+
+#[derive(Insertable)]
+#[table_name="books"]
+pub struct NewBook {
   pub title: String,
   pub author: String,
   pub published: bool,
