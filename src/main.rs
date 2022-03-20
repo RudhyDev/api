e6a7c15272e986bc2892657dda6e5f75aad7eba5
RudhyDev <rudhy.pereira@gmail.com>
2022-03-20 18:37:17 -0300

    first

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate diesel
+#[macro_use]
+extern crate diesel_codegen;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+mod schema;
+mod models;
+
+
+fn main() {
+    println!("Hello, world!");
+}
