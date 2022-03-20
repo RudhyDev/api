6196b4b23ceec31a6399b0db27ed66603461f33e
RudhyDev <rudhy.pereira@gmail.com>
2022-03-20 19:25:26 -0300

    feat: create code to run in main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 #[macro_use]
-extern crate diesel
-#[macro_use]
+extern crate diesel;
 extern crate diesel_codegen;
 extern crate dotenv;
 
 use dotenv::dotenv;
 use std::env;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
 
 mod schema;
 mod models;
 
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let conn = PgConnection::establish(&database_url).unwrap();
+
+    let book = models::NewBook {
+        title: String::from("Meu livro 01"),
+        author: String::from("Rudhy Pereira"),
+        published: true,
+    };
+
+    if models::Book::insert(book, &conn){
+        println!("Success!");
+    } else {
+        println!("Failed!");
+    }
 }
