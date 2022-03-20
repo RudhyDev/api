c3ea99f23c715d716883021ef890ae833e4fd5ac
RudhyDev <rudhy.pereira@gmail.com>
2022-03-20 19:05:18 -0300

    feat: create all the functions inside models

--- src/models.rs
+++ src/models.rs
@@ -20,3 +20,59 @@ pub struct NewBook {
   pub author: String,
   pub published: bool,
 }
+
+impl Book {
+  pub fn show(id: i32, conn: &PgConnection) -> Vec<Book> {
+    all_books
+      .find(id)
+      .load::<Book>(conn)
+      .expect("Error loading book")
+  }
+
+  pub fn all(conn: &PgConnection) -> Vec<Book> {
+    all_books
+      .order(books::id.desc())
+      .load::<Book>(conn)
+      .expect("Error loading books")
+  }
+
+  pub fn update_by_id(id: i32, conn: &PgConnection, book: NewBook) -> bool {
+    use schema::books::dsl::{author as a, published as p, title as t};
+    let NewBook {
+      title,
+      author,
+      published,
+    } = book;
+
+    diesel::update(all_books.find(id))
+      .set((t.eq(title), a.eq(author), p.eq(published)))
+      .get_result::<Book>(conn)
+      .is_ok()
+
+  }
+
+  pub fn insert(book: NewBook, conn: &PgConnection) -> bool {
+    diesel::insert_into(books::table)
+      .values(&book)
+      .execute(conn)
+      .is_ok()
+  }
+
+  pub fn delete_by_id(id: i32, conn: &PgConnection) -> bool {
+    if Book::show(id, conn).is_empty() {
+      return false;
+    };
+    diesel::delete(all_books.find(id))
+      .execute(conn)
+      .is_ok()
+  }
+
+  pub all_by_author(author: String, conn: &PgConnection) -> Vec<Book> {
+    all_books
+      .filter(books::author.eq(author))
+      .load::<Book>(conn)
+      .expect("Error loading books by author")
+  }
+
+
+}
